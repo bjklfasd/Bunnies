fa3880f31f7e44e3eabc65c0808ce2516a3e69ab
bjklfasd <br3akerz@outlook.com>
2026-07-23 00:00:19 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 const express = require("express");
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const { createBareServer } = require("@tomphttp/bare-server-node");
 
 const app = express();
 
-app.use(
-  "/",
-  createProxyMiddleware({
-    target: "https://frogiesarcade.win", 
-    changeOrigin: true,
-    ws: true,
-  })
-);
+const bare = createBareServer("/bare/");
 
-app.listen(process.env.PORT || 3000);
+app.use((req, res, next) => {
+  if (bare.shouldRoute(req)) {
+    return bare.routeRequest(req, res);
+  }
+
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("Bare server is running!");
+});
+
+const server = app.listen(process.env.PORT || 3000, () => {
+  console.log("Bare server running");
+});
+
+server.on("upgrade", (req, socket, head) => {
+  if (bare.shouldRoute(req)) {
+    bare.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
+});
